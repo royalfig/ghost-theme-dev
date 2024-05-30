52d6db1a07aa361ffde30a307b92b55f70102295
Ryan Feigenbaum <48868107+royalfig@users.noreply.github.com>
2024-05-30 13:26:15 -0400

    Add default content

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 #!/usr/bin/env node
 
-import select from '@inquirer/select';
-import chalk from 'chalk';
-import chokidar from 'chokidar';
-import * as esbuild from 'esbuild';
-import { execSync } from 'node:child_process';
-import { readdir, stat } from 'node:fs/promises';
-import { join } from 'node:path';
-import { argv } from 'node:process';
-import WebSocket, { WebSocketServer } from 'ws';
+import select from "@inquirer/select";
+import chalk from "chalk";
+import chokidar from "chokidar";
+import * as esbuild from "esbuild";
+import { execSync } from "node:child_process";
+import { readdir, writeFile, stat, access, mkdir } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { argv } from "node:process";
+import WebSocket, { WebSocketServer } from "ws";
 
 /* eslint-disable no-console */
+
+const defaultTemplateContent = `<!DOCTYPE html>
+<html lang="{{@site.locale}}">
+<head>
+    {{!-- Basic meta - advanced meta is output with {{ghost_head}} below --}}
+    <title>{{meta_title}}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    {{!-- Theme assets - use the {{asset}} helper to reference styles & scripts, this will take care of caching and cache-busting automatically --}}
+    <link rel="stylesheet" type="text/css" href="{{asset "built/css/index.css"}}">
+    <script src="{{asset "built/js/index.js"}}" defer></script>
+
+    {{!-- This tag outputs all your advanced SEO meta, structured data, and other important settings, it should always be the last tag before the closing head tag --}}
+    {{ghost_head}}
+</head>
+<body class="{{body_class}}">
+
+</body>
+</html>`;
+
+const postTemplateContent = `{{!< default}}
+{{!-- The tag above means: insert everything in this file into the body of the default.hbs template --}}
+
+{{#post}}
+{{!-- Learn more: https://ghost.org/docs/themes/helpers/post/ --}}
+{{/post}}`;
+
+const indexTemplateContent = `{{!< default}}
+{{!-- The tag above means: insert everything in this file into the body of the default.hbs template --}}
+
+{{#foreach posts}}
+{{!-- Learn more: https://ghost.org/docs/themes/contexts/index-context/ --}}
+{{/foreach}}`;
 
 let url;
 
@@ -24,30 +58,71 @@ function runCommand(command) {
   }
 }
 
+async function makeSkeleton() {
+  const filesToCheck = [
+    { filename: "index.hbs", content: indexTemplateContent },
+    { filename: "post.hbs", content: postTemplateContent },
+    { filename: "default.hbs", content: defaultTemplateContent },
+    { filename: "assets/css/index.css", content: null },
+    { filename: "assets/js/index.js", content: null },
+  ];
+  const folderPath = process.cwd();
+  try {
+    const results = await Promise.all(
+      filesToCheck.map(async (file) => {
+        const dir = dirname(join(folderPath, file.filename));
+        try {
+          await access(join(folderPath, file.filename));
+        } catch (error) {
+          if (file.filename.endsWith(".js")) {
+            try {
+              await access(join(folderPath, "assets/js/index.ts"));
+            } catch (error) {
+              await mkdir(dir, { recursive: true });
+              await writeFile(
+                join(folderPath, file.filename),
+                file.content ? file.content : ""
+              );
+            }
+          } else {
+            await mkdir(dir, { recursive: true });
+            await writeFile(
+              join(folderPath, file.filename),
+              file.content ? file.content : ""
+            );
+          }
+        }
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function symLinkTheme() {}
 
 async function parseGhostCliOutput() {
-  const result = runCommand('ghost ls');
-  const localUrl = result.match(/http:\/\/localhost:\d+/g);
+  const result = runCommand("ghost ls");
+  const localUrl = result.match(/http:\/\/(localhost|127\.0\.0\.1):\d+/g);
 
   if (!localUrl) {
     console.log(
-      'No running Ghost instance found. Please start Ghost and try again. To start Ghost, run `ghost start` in your Ghost installation directory.',
+      "No running Ghost instance found. Please start Ghost and try again. To start Ghost, run `ghost start` in your Ghost installation directory."
     );
     process.exit(1);
   }
-
-  if (localUrl.length > 0) {
+  if (localUrl.length > 1) {
+    console.log(localUrl);
     const details = await Promise.all(
       localUrl.map(async (u) => {
-        const output = runCommand(`curl -s ${u}`) || '';
+        const output = runCommand(`curl -s ${u}`) || "";
         const title = output.match(/<title>(.*?)<\/title>/);
-        return { url: u, title: title[1] };
-      }),
+        return { url: u, title: title?.[1] };
+      })
     );
 
     const answer = await select({
-      message: 'More than one Ghost site running. Choose one:',
+      message: "More than one Ghost site running. Choose one:",
       choices: details.map((u) => ({
         name: `${u.title} (${u.url})`,
         value: u.url,
@@ -60,20 +135,20 @@ async function parseGhostCliOutput() {
 }
 
 function formatBytes(bytes, decimals = 2) {
-  if (!+bytes) return '0 Bytes';
+  if (!+bytes) return "0 Bytes";
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = [
-    'Bytes',
-    'KiB',
-    'MiB',
-    'GiB',
-    'TiB',
-    'PiB',
-    'EiB',
-    'ZiB',
-    'YiB',
+    "Bytes",
+    "KiB",
+    "MiB",
+    "GiB",
+    "TiB",
+    "PiB",
+    "EiB",
+    "ZiB",
+    "YiB",
   ];
 
   const i = Math.floor(Math.log(bytes) / Math.log(k));
@@ -81,10 +156,10 @@ function formatBytes(bytes, decimals = 2) {
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
 
-const isWatch = argv.includes('--watch');
+const isWatch = argv.includes("--watch");
 
-const watcher = chokidar.watch('.', {
-  ignored: [/(^|[\/\\])\../, '**/built/**', 'dev.js', 'node_modules'], // ignore dotfiles
+const watcher = chokidar.watch(".", {
+  ignored: [/(^|[\/\\])\../, "**/built/**", "dev.js", "node_modules"], // ignore dotfiles
 });
 
 async function findFilesRecursively(directory) {
@@ -115,8 +190,7 @@ async function findEntryPoints(entryPointPath, exts) {
   try {
     const files = await findFilesRecursively(directory);
     const entryPoints = files.filter(
-      (file) =>
-        file.includes('index') && exts.some((ext) => file.endsWith(ext)),
+      (file) => file.includes("index") && exts.some((ext) => file.endsWith(ext))
     );
     return entryPoints;
   } catch (error) {
@@ -127,31 +201,32 @@ async function findEntryPoints(entryPointPath, exts) {
 function printCompilationDetails(content, change, firstTime = false) {
   if (firstTime) {
     console.log(chalk.dim(`┏━ Building...`));
-    console.log(chalk.dim('┃'));
+    console.log(chalk.dim("┃"));
   } else {
     console.log(chalk.dim(`┏━ Change detected. Rebuilding... `));
-    console.log(chalk.dim('┃'));
+    console.log(chalk.dim("┃"));
   }
 
   content.results.forEach(({ file, value }) => {
     console.log(
       chalk.dim(`┃`),
       ` ${
-        change ? change + chalk.bold.magenta(' → ') : ''
-      }${file} (${chalk.bold.magenta(value)})`,
+        change ? change + chalk.bold.magenta(" → ") : ""
+      }${file} (${chalk.bold.magenta(value)})`
     );
   });
-  console.log(chalk.dim('┃'));
+  console.log(chalk.dim("┃"));
   console.log(
-    `${chalk.dim('┗━ Done in')} ${content.time.toFixed(2)}ms ${chalk.dim(
-      'at',
-    )} ${new Date().toLocaleTimeString(undefined, 'short')}\n`,
+    `${chalk.dim("┗━ Done in")} ${content.time.toFixed(2)}ms ${chalk.dim(
+      "at"
+    )} ${new Date().toLocaleTimeString(undefined, "short")}\n`
   );
 }
 
 async function init() {
-  const jsEntryPoints = await findEntryPoints('assets/js', ['.ts', '.js']);
-  const cssEntryPoints = await findEntryPoints('assets/css', ['.css']);
+  await makeSkeleton();
+  const jsEntryPoints = await findEntryPoints("assets/js", [".ts", ".js"]);
+  const cssEntryPoints = await findEntryPoints("assets/css", [".css"]);
   const res = await writeAssets([...jsEntryPoints, ...cssEntryPoints]);
 
   if (isWatch) {
@@ -159,7 +234,7 @@ async function init() {
     initWs(jsEntryPoints, cssEntryPoints, res);
   } else {
     printCompilationDetails(res);
-    console.log(chalk.green('⬥'), '  Files built successfully. Exiting...');
+    console.log(chalk.green("⬥"), "  Files built successfully. Exiting...");
     process.exit(0);
   }
 }
@@ -188,13 +263,13 @@ async function writeAssets(jsEntryPoints) {
   const buildOptions = {
     entryPoints: jsEntryPoints,
     bundle: true,
-    outbase: 'assets',
-    outdir: 'assets/built',
+    outbase: "assets",
+    outdir: "assets/built",
     minify: !isWatch,
     sourcemap: true,
     metafile: true,
-    target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
-    external: ['*.woff', '*.woff2'],
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+    external: ["*.woff", "*.woff2"],
   };
 
   let results = [];
@@ -203,7 +278,7 @@ async function writeAssets(jsEntryPoints) {
     const buildOptionsClone = { ...buildOptions };
     buildOptionsClone.entryPoints = [entry];
 
-    if (isWatch && entry.includes('assets/js/index')) {
+    if (isWatch && entry.includes("assets/js/index")) {
       buildOptionsClone.footer = {
         js: footerScript,
       };
@@ -217,13 +292,13 @@ async function writeAssets(jsEntryPoints) {
 
     const vals = Object.entries(res.metafile.outputs).reduce(
       (acc, [key, value]) => {
-        if (key.includes('map')) {
+        if (key.includes("map")) {
           return acc;
         }
         acc.push({ file: key, value: formatBytes(value.bytes) });
         return acc;
       },
-      [],
+      []
     );
 
     results.push(...vals);
@@ -236,32 +311,32 @@ let siteData = false;
 
 function printHeader(connectionError = false, firstConnection = false) {
   console.clear();
-  console.log(`${chalk.bold.green('●')}  Ghost theme dev server running...`);
+  console.log(`${chalk.bold.green("●")}  Ghost theme dev server running...`);
 
   if (connectionError) {
     console.log(
-      chalk.redBright.bold('✘  No connection. '),
-      `Try refreshing your browser or visiting ${chalk.underline.blue(url)}.`,
+      chalk.redBright.bold("✘  No connection. "),
+      `Try refreshing your browser or visiting ${chalk.underline.blue(url)}.`
     );
   } else if (firstConnection) {
     console.log(
-      chalk.blueBright.bold('➜'),
-      ` Connected to ${chalk.underline.blue(url)}.`,
+      chalk.blueBright.bold("➜"),
+      ` Connected to ${chalk.underline.blue(url)}.`
     );
   } else if (siteData.version) {
     console.log(
-      chalk.blueBright.bold('➜'),
+      chalk.blueBright.bold("➜"),
       ` Connected to ${chalk.underline.blue(siteData.url)} ${chalk.dim(
-        `(${siteData.version})`,
-      )}`,
+        `(${siteData.version})`
+      )}`
     );
   } else {
     console.log(
-      chalk.blueBright.bold('➜'),
-      ` Visit ${chalk.underline.blue(url)} to see your changes live.`,
+      chalk.blueBright.bold("➜"),
+      ` Visit ${chalk.underline.blue(url)} to see your changes live.`
     );
   }
-  console.log('');
+  console.log("");
 }
 async function initWs(jsEntry, cssEntry, res) {
   const int = setTimeout(() => {
@@ -277,19 +352,19 @@ async function initWs(jsEntry, cssEntry, res) {
 
   let firstConnection = true;
 
-  watcher.on('all', async (event, path) => {
-    if (path.endsWith('.hbs')) {
+  watcher.on("all", async (event, path) => {
+    if (path.endsWith(".hbs")) {
       printHeader(false, false);
       console.log(chalk.cyanBright(`{{  `), `${path} changed. Reloading...`);
 
       wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
-          client.send('HBS changed: ' + path);
+          client.send("HBS changed: " + path);
         }
       });
     }
 
-    if (path.endsWith('.css') || path.endsWith('.js') || path.endsWith('.ts')) {
+    if (path.endsWith(".css") || path.endsWith(".js") || path.endsWith(".ts")) {
       let rootFile = path;
 
       if (!/index\.(css|js|ts)$/.test(path)) {
@@ -309,7 +384,7 @@ async function initWs(jsEntry, cssEntry, res) {
     }
   });
 
-  wss.on('connection', (ws) => {
+  wss.on("connection", (ws) => {
     clearTimeout(int);
 
     if (firstConnection) {
@@ -317,7 +392,7 @@ async function initWs(jsEntry, cssEntry, res) {
       firstConnection = false;
     }
 
-    ws.on('message', (message) => {
+    ws.on("message", (message) => {
       const { url, title, version } = JSON.parse(message);
       siteData = { url, title, version };
     });
